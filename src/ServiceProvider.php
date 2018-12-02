f6625e15dc8920e4722404d72f453bd44ec398ad
Justin Hayes <justin@soapboxhq.com>
2018-12-02 01:55:09 -0500

    Add the config file

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,6 +7,16 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 class ServiceProvider extends BaseServiceProvider
 {
     /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/../config/datadog.php' => config_path('datadog.php')]);
+    }
+
+    /**
      * Register any package services.
      *
      * @return void
